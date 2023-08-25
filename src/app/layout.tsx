211aa5428a92677d30748ee31a50b8e305014ba9
shardel23 <shardel23@gmail.com>
2023-08-26 00:57:16 +0300

    Reduce padding in mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark w-full">
       <body className={inter.className + " w-full"}>
-        <div className="flex-1 space-y-4 p-8 pt-6 w-full">
-          <div className="font-bold md:text-4xl">
+        <div className="flex-1 space-y-4 p-3 md:p-8 md:pt-6 w-full">
+          <div className="font-bold text-xl md:text-4xl">
             <Link href={`/`}>MTG Collection</Link>
           </div>
           <div className="flex w-full flex-col md:flex-row">
