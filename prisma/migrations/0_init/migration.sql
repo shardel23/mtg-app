5edbbc503c23d495c4d86949c5af9ae1150c1d6b
shardel23 <shardel23@gmail.com>
2023-08-31 10:16:09 +0300

    Changes to prisma migrate

---
+++ prisma/migrations/0_init/migration.sql
@@ -0,0 +1,31 @@
+-- CreateTable
+CREATE TABLE "Card" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "setId" TEXT NOT NULL,
+    "setName" TEXT NOT NULL,
+    "setCode" TEXT NOT NULL,
+    "setIconSvgUri" TEXT NOT NULL,
+    "rarity" TEXT NOT NULL,
+    "collectorNumber" INTEGER NOT NULL,
+    "imageUri" TEXT NOT NULL,
+    "isCollected" BOOLEAN NOT NULL DEFAULT false,
+    "isFoil" BOOLEAN NOT NULL DEFAULT false,
+    "albumId" INTEGER,
+
+    CONSTRAINT "Card_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Album" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "setId" TEXT,
+    "setName" TEXT,
+
+    CONSTRAINT "Album_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Card" ADD CONSTRAINT "Card_albumId_fkey" FOREIGN KEY ("albumId") REFERENCES "Album"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
