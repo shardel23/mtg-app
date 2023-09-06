2e0ef2414fc0fb14a627e8fcf54d07262b55a185
shardel23 <shardel23@gmail.com>
2023-09-07 00:12:51 +0300

    Add collection state indication in header for mobile

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -16,23 +16,30 @@ async function Header() {
   const collection = await getCollection();
   const collections = await getAllCollections();
   return (
-    <div className="flex justify-between">
-      <div className="flex items-center justify-between gap-x-2 md:justify-normal md:gap-x-8">
-        <div className="font-bold text-xl w-24 md:text-4xl md:w-72">
-          <Link href={`/`}>MTG Collection</Link>
-        </div>
-        <CardSearch />
+    <div className="flex w-full justify-between">
+      <div className="flex font-bold text-2xl w-24 md:text-4xl md:w-72">
+        <Link href={`/`}>MTG Collection</Link>
       </div>
-      <div className="flex items-center">
-        <div className="w-48 hidden md:flex items-center gap-x-4">
-          <Label>Collection: </Label>
-          <CollectionSelector
-            collections={collections}
-            initialCollection={collection}
-          />
+      <div className="flex flex-col gap-y-2">
+        <div className="flex md:hidden justify-end text-xs">
+          {`Collection: ${collection}`}
         </div>
-        <div className="flex md:hidden">
-          <HamburgerMenu sets={sets} albums={albums} collection={collection} />
+        <div className="flex items-center justify-end md:gap-x-8">
+          <CardSearch />
+          <div className="w-48 hidden md:flex items-center gap-x-4">
+            <Label>Collection: </Label>
+            <CollectionSelector
+              collections={collections}
+              initialCollection={collection}
+            />
+          </div>
+          <div className="flex md:hidden">
+            <HamburgerMenu
+              sets={sets}
+              albums={albums}
+              collection={collection}
+            />
+          </div>
         </div>
       </div>
     </div>
