60cce07d0c4499774e4e355a62d83a16659e6848
shardel23 <shardel23@gmail.com>
2023-09-12 13:24:39 +0300

    Bugfix - Add key to mapped elements

--- src/components/loadingGrid.tsx
+++ src/components/loadingGrid.tsx
@@ -4,7 +4,7 @@ export default function LoadingGrid() {
   return (
     <div className={`grid grid-cols-3 md:grid-cols-5 gap-1`}>
       {[...Array(15)].map((_, i) => (
-        <LoadingBlock className="w-full h-0 pb-[125%]" />
+        <LoadingBlock key={i} className="w-full h-0 pb-[125%]" />
       ))}
     </div>
   );
