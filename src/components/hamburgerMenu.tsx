c1811a80a376749add260f1947ee47d2e6dfc622
shardel23 <shardel23@gmail.com>
2023-10-31 11:22:27 +0200

    Hide collection choosing feature

--- src/components/hamburgerMenu.tsx
+++ src/components/hamburgerMenu.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { setCollectionCookie } from "@/actions/cookieActions";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -14,30 +13,20 @@ import {
 import { AlbumData, SetData } from "@/types/types";
 import { User } from "next-auth";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useTransition } from "react";
 import CreateNewAlbumDialog from "./createNewAlbumDialog";
 import Hamburger from "./icons/hamburger";
 import SignOut from "./signOut";
-import { Label } from "./ui/label";
 import { ScrollArea } from "./ui/scroll-area";
 
 function HamburgerMenu({
   user,
   sets,
   albums,
-  collection,
 }: {
   user: User | undefined;
   sets: SetData[];
   albums: AlbumData[];
-  collection: string;
 }) {
-  const [_, startTransition] = useTransition();
-  const router = useRouter();
-
-  const collections = ["Default"];
-
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -68,37 +57,8 @@ function HamburgerMenu({
               </DropdownMenuItem>
             ))}
           </ScrollArea>
-          {/* <DropdownMenuItem>
-            <User className="mr-2 h-4 w-4" />
-            <span>Profile</span>
-            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-          </DropdownMenuItem> */}
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuLabel>Collection</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <ScrollArea viewportClassName="max-h-36">
-            {collections.map((collectionName) => (
-              <DropdownMenuItem key={collectionName}>
-                <Label
-                  className="mr-2"
-                  onClick={() => {
-                    startTransition(async () => {
-                      await setCollectionCookie(collectionName);
-                      router.push("/");
-                    });
-                  }}
-                >
-                  {collectionName}
-                </Label>
-                {collection === collectionName && (
-                  <span className="text-green-500">✓</span>
-                )}
-              </DropdownMenuItem>
-            ))}
-          </ScrollArea>
-        </DropdownMenuGroup>
       </DropdownMenuContent>
     </DropdownMenu>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,23 +1,14 @@
-import {
-  getAllAlbums,
-  getAllCollections,
-  getAllSets,
-  getCollection,
-} from "@/actions/mtgActions";
+import { getAllAlbums, getAllSets } from "@/actions/mtgActions";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth/next";
 import Link from "next/link";
 import Auth from "./auth";
 import CardSearch from "./cardSearch";
 import HamburgerMenu from "./hamburgerMenu";
-import CollectionSelector from "./selectors/collectionSelector";
-import { Label } from "./ui/label";
 
 async function Header() {
   const sets = await getAllSets();
   const albums = await getAllAlbums();
-  const collection = await getCollection();
-  const collections = await getAllCollections();
   const session = await getServerSession(authOptions);
   const user = session?.user;
 
@@ -31,26 +22,11 @@ async function Header() {
           <Auth />
         </div>
       </div>
-      <div className="flex flex-col gap-y-2">
-        <div className="flex justify-end text-xs md:hidden">
-          {`Collection: ${collection}`}
-        </div>
+      <div className="flex flex-col gap-y-2 justify-center">
         <div className="flex items-center justify-end md:gap-x-8">
           <CardSearch />
-          <div className="hidden w-48 items-center gap-x-4 md:flex">
-            <Label>Collection: </Label>
-            <CollectionSelector
-              collections={collections}
-              initialCollection={collection}
-            />
-          </div>
           <div className="flex md:hidden">
-            <HamburgerMenu
-              user={user}
-              sets={sets}
-              albums={albums}
-              collection={collection}
-            />
+            <HamburgerMenu user={user} sets={sets} albums={albums} />
           </div>
         </div>
       </div>
