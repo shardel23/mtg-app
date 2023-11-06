8a945493366c377f6c6c1192c7cd4aa1b80c0c53
shardel23 <shardel23@gmail.com>
2023-11-06 21:14:47 +0200

    Switch Response to NextResponse

--- src/app/api/card/route.ts
+++ src/app/api/card/route.ts
@@ -1,8 +1,9 @@
 import { getCard } from "@/lib/scryfallApi";
+import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const cardId = searchParams.get("cardId");
   const price = (await getCard(cardId!)).prices.usd;
-  return Response.json({ price });
+  return NextResponse.json({ price });
 }
