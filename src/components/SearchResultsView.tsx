443a7e45aa6cae929758eee0a1668f54b8707581
shardel23 <shardel23@gmail.com>
2023-11-27 14:11:20 +0200

    Add title and description to tradeable cards view

--- src/components/SearchResultsView.tsx
+++ src/components/SearchResultsView.tsx
@@ -10,23 +10,34 @@ function SearchResultsView({
   results: Map<string, CardData[]>;
   query?: string;
 }) {
-  const displayMessage = query
+  const isSearch = query != null;
+  const displayMessage = isSearch
     ? `Displaying ${results.size} cards where the name includes "${query}"`
     : `Displaying ${results.size} cards available for trade`;
   return (
-    <div className="flex flex-col">
-      <div
-        data-testid="search-page-display-message-div"
-        className="border-b border-t pb-2 pt-2 text-center text-xxs md:pl-4 md:text-left"
-      >
-        {displayMessage}
+    <div className="flex flex-col gap-y-8">
+      {!isSearch && (
+        <div className="flex flex-col gap-y-4">
+          <h1 className="text-2xl">Tradeable cards</h1>
+          <div>
+            Tradeable cards are cards in your collection with more than 1 copy
+          </div>
+        </div>
+      )}
+      <div>
+        <div
+          data-testid="search-page-display-message-div"
+          className="border-b border-t pb-2 pt-2 text-center text-xxs md:pl-4 md:text-left"
+        >
+          {displayMessage}
+        </div>
+        <CardGrid
+          cards={results}
+          cardsPerRow={5}
+          viewMode="edit"
+          isCardDeleteable={false}
+        />
       </div>
-      <CardGrid
-        cards={results}
-        cardsPerRow={5}
-        viewMode="edit"
-        isCardDeleteable={false}
-      />
     </div>
   );
 }
