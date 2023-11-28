4e058b4677475f8968c557bd9006e41683342122
shardel23 <shardel23@gmail.com>
2023-11-28 10:59:00 +0200

    Add new path (with username) to view albums

---
+++ src/app/u/[username]/[albumId]/page.tsx
@@ -0,0 +1,23 @@
+import { getAlbumCards } from "@/actions/mtgActions";
+import AlbumView from "@/app/album/[albumId]/AlbumView";
+import RedirectIfNotLoggedIn from "@/components/redirect";
+
+type PageParams = { username: string; albumId: string };
+
+export default async function AlbumPage({ params }: { params: PageParams }) {
+  const { album, cards, viewMode } = await getAlbumCards(params.albumId);
+  if (album == null) {
+    return (
+      <>
+        <RedirectIfNotLoggedIn />
+        <div className="text-center"> Album not found </div>
+      </>
+    );
+  }
+  return (
+    <>
+      <RedirectIfNotLoggedIn />
+      <AlbumView album={album} cards={cards} viewMode={viewMode} />
+    </>
+  );
+}
