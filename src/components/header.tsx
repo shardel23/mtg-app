401cc7641d7a9d0ca912d30d0827e5b76787a656
Shahar Dotan <shardel23@gmail.com>
2023-11-30 20:27:42 +0200

    App's banner make all text clickable

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -16,14 +16,10 @@ async function Header() {
     <div className="fixed top-0 z-10 flex h-24 w-full justify-between border-b-2 bg-slate-950 p-3 md:p-8 md:pt-6">
       <div className="flex items-center gap-x-8">
         <div className="flex flex-col w-48 md:w-64">
-          <Link
-            className="text-2xl md:text-4xl font-bold"
-            data-testid="app-banner"
-            href={`/`}
-          >
-            SuperMTG
+          <Link data-testid="app-banner" href={`/`}>
+            <div className="text-2xl md:text-4xl font-bold">SuperMTG</div>
+            <div className="text-sm md:text-lg">Your collection manager</div>
           </Link>
-          <div className="text-sm md:text-lg">Your collection manager</div>
         </div>
         <div className="hidden md:flex">
           <Auth />
