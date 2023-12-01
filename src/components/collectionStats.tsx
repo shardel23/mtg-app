28c5a93e7f922e1dcb7c61d731c33bbca7fbec67
shardel23 <shardel23@gmail.com>
2023-12-01 19:15:30 +0200

    Avoid rendering stats section title when there are no albums of that category

--- src/components/collectionStats.tsx
+++ src/components/collectionStats.tsx
@@ -91,77 +91,83 @@ const CollectionGrid: React.FC<CollectionGridProps> = ({
 
   return (
     <div className="flex flex-col gap-y-8 w-full justify-center items-center">
-      <div className="flex flex-col w-full items-center gap-y-4">
-        <div className="text-xl font-bold underline">Set Albums</div>
-        <div className="grid px-2 grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 w-full">
-          {collectionData.setAlbumsStats.map((album) => {
-            const percentage =
-              (album.total.collected / album.total.total) * 100;
-            const isChosenAlbum = album.name === chosenAlbum?.name;
-            return (
-              <div
-                key={album.name}
-                className={
-                  "flex flex-col gap-y-2 rounded-lg border-2 bg-slate-500 p-4 shadow-md hover:cursor-pointer hover:bg-slate-600 " +
-                  (isChosenAlbum ? "border-slate-200" : "")
-                }
-                onClick={() => {
-                  if (isMobile) {
-                    router.push(`/album/${album.id}`);
-                  } else {
-                    setChosenAlbum(album);
+      {collectionData.setAlbumsStats.length > 0 && (
+        <div className="flex flex-col w-full items-center gap-y-4">
+          <div className="text-xl font-bold underline">Set Albums</div>
+          <div className="grid px-2 grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 w-full">
+            {collectionData.setAlbumsStats.map((album) => {
+              const percentage =
+                (album.total.collected / album.total.total) * 100;
+              const isChosenAlbum = album.name === chosenAlbum?.name;
+              return (
+                <div
+                  key={album.name}
+                  className={
+                    "flex flex-col gap-y-2 rounded-lg border-2 bg-slate-500 p-4 shadow-md hover:cursor-pointer hover:bg-slate-600 " +
+                    (isChosenAlbum ? "border-slate-200" : "")
                   }
-                }}
-              >
-                <h2 className="mb-2 truncate text-lg font-bold">
-                  {album.name}
-                </h2>
-                <p className="mb-2">
-                  Collected: {album.total.collected} / {album.total.total}
-                </p>
-                <div className="relative pt-1">
-                  <div className="mb-4 flex h-2 overflow-hidden rounded bg-gray-200 text-xs">
-                    <div
-                      style={{
-                        width: `${percentage}%`,
-                      }}
-                      className="flex flex-col justify-center whitespace-nowrap bg-green-500 text-center text-white shadow-none"
-                    ></div>
+                  onClick={() => {
+                    if (isMobile) {
+                      router.push(`/album/${album.id}`);
+                    } else {
+                      setChosenAlbum(album);
+                    }
+                  }}
+                >
+                  <h2 className="mb-2 truncate text-lg font-bold">
+                    {album.name}
+                  </h2>
+                  <p className="mb-2">
+                    Collected: {album.total.collected} / {album.total.total}
+                  </p>
+                  <div className="relative pt-1">
+                    <div className="mb-4 flex h-2 overflow-hidden rounded bg-gray-200 text-xs">
+                      <div
+                        style={{
+                          width: `${percentage}%`,
+                        }}
+                        className="flex flex-col justify-center whitespace-nowrap bg-green-500 text-center text-white shadow-none"
+                      ></div>
+                    </div>
                   </div>
+                  <p className="text-sm">
+                    {percentage.toPrecision(3)}% Complete
+                  </p>
                 </div>
-                <p className="text-sm">{percentage.toPrecision(3)}% Complete</p>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col w-full items-center gap-y-4">
-        <div className="text-center text-xl font-bold underline">
-          Custom Albums
-        </div>
-        <div className="grid w-full px-2 grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
-          {collectionData.nonSetAlbumsStats.map((album) => {
-            return (
-              <div
-                key={album.name}
-                className={
-                  "flex flex-col gap-y-2 rounded-lg border-2 bg-slate-500 p-4 shadow-md hover:cursor-pointer hover:bg-slate-600"
-                }
-                onClick={() => {
-                  if (isMobile) {
-                    router.push(`/album/${album.id}`);
+      )}
+      {collectionData.nonSetAlbumsStats.length > 0 && (
+        <div className="flex flex-col w-full items-center gap-y-4">
+          <div className="text-center text-xl font-bold underline">
+            Custom Albums
+          </div>
+          <div className="grid w-full px-2 grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+            {collectionData.nonSetAlbumsStats.map((album) => {
+              return (
+                <div
+                  key={album.name}
+                  className={
+                    "flex flex-col gap-y-2 rounded-lg border-2 bg-slate-500 p-4 shadow-md hover:cursor-pointer hover:bg-slate-600"
                   }
-                }}
-              >
-                <h2 className="mb-2 truncate text-lg font-bold">
-                  {album.name}
-                </h2>
-                <p className="mb-2">Card Count: {album.total.collected}</p>
-              </div>
-            );
-          })}
+                  onClick={() => {
+                    if (isMobile) {
+                      router.push(`/album/${album.id}`);
+                    }
+                  }}
+                >
+                  <h2 className="mb-2 truncate text-lg font-bold">
+                    {album.name}
+                  </h2>
+                  <p className="mb-2">Card Count: {album.total.collected}</p>
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
