3eb6432a7b536e33652fbe73a8361fe985f51c5b
shardel23 <shardel23@gmail.com>
2024-02-17 09:58:38 +0200

    Integrate with inngest to fetch prices

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,11 +1,13 @@
-import fetchPrices from "@/lib/prices";
+import { inngest } from "@/lib/ingest/client";
 import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
 export async function GET() {
   try {
-    await fetchPrices();
+    await inngest.send({
+      name: "fetch.prices",
+    });
   } catch (error) {
     return NextResponse.json({
       status: "error",
