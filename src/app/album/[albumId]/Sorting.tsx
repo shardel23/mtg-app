e82ecefea2d0fdd095b838caf0809304c17e8b99
shardel23 <shardel23@gmail.com>
2024-02-18 14:52:22 +0200

    Add indication to the selected sorting direction

--- src/app/album/[albumId]/Sorting.tsx
+++ src/app/album/[albumId]/Sorting.tsx
@@ -11,9 +11,12 @@ import {
 } from "@/components/ui/select";
 import { Dispatch, SetStateAction } from "react";
 
+type SortingDirection = "asc" | "desc";
+
 type SortingProps = {
   setSortingMethod: Dispatch<SetStateAction<keyof typeof SORTINGS>>;
-  setSortingDirection: Dispatch<SetStateAction<"asc" | "desc">>;
+  setSortingDirection: Dispatch<SetStateAction<SortingDirection>>;
+  sortingDirection: SortingDirection;
 };
 
 const sortings = Array.from(Object.keys(SORTINGS)) as (keyof typeof SORTINGS)[];
@@ -21,6 +24,7 @@ const sortings = Array.from(Object.keys(SORTINGS)) as (keyof typeof SORTINGS)[];
 export default function Sorting({
   setSortingMethod,
   setSortingDirection,
+  sortingDirection,
 }: SortingProps) {
   return (
     <div className="flex flex-col w-48 gap-y-1">
@@ -28,14 +32,20 @@ export default function Sorting({
         <div className="text-sm">Sort by:</div>
         <div className="flex gap-x-2">
           <ArrowUp
-            className="hover:cursor-pointer"
+            className={
+              "hover:cursor-pointer" +
+              (sortingDirection === "asc" ? " text-blue-500" : "")
+            }
             onClick={() => {
               setSortingDirection("asc");
             }}
           />
           /
           <ArrowDown
-            className="hover:cursor-pointer"
+            className={
+              "hover:cursor-pointer" +
+              (sortingDirection === "desc" ? " text-blue-500" : "")
+            }
             onClick={() => {
               setSortingDirection("desc");
             }}
