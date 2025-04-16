029d219264acd98dec04fe4f2362d13928d7f4d0
shardel23 <shardel23@gmail.com>
2025-04-16 22:38:30 +0300

    Redesigned sign-in page

--- src/app/signin/SignIn.tsx
+++ src/app/signin/SignIn.tsx
@@ -23,12 +23,20 @@ export default function SignIn() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center gap-y-6">
-      <h1 className="text-4xl"> MTG Collection App </h1>
-      {providers &&
-        Object.values(providers).map((provider) => (
-          <Provider key={provider.name} provider={provider} />
-        ))}
+    <div className="flex flex-col items-center gap-y-8 max-w-lg w-full px-4">
+      <div className="flex flex-col items-center gap-y-4 text-center">
+        <h1 className="text-4xl font-bold md:text-5xl">Welcome to SuperMTG</h1>
+        <p className="text-xl text-slate-400">
+          Sign in to start managing your MTG collection
+        </p>
+      </div>
+
+      <div className="w-full flex flex-col gap-y-4">
+        {providers &&
+          Object.values(providers).map((provider) => (
+            <Provider key={provider.name} provider={provider} />
+          ))}
+      </div>
     </div>
   );
 }
